8c1ff4b113f81bf35189620bd74ae6fd3c91260b
Aaryan Raj <aaryan_7@Aaryan-Raj.localdomain>
2025-06-19 12:51:46 +0000

    static_update

--- Static-Variables/static.c
+++ Static-Variables/static.c
@@ -11,3 +11,23 @@ int add() {
     sum++;
     printf("\n Sum is %d", sum);
 }
+
+/*OR
+#include <stdio.h>
+
+int add();
+
+int main() {
+    int i, ret;
+    for (i = 1; i <= 10; i++) {
+        ret = add();
+    }
+    return 0;
+}
+int add() {
+    static int sum = 10;
+    sum++;
+    printf("\n Sum is %d", sum);
+    return sum;
+}
+*/
